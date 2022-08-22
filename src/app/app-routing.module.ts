898ccbf2ea9534a5c564056e41b72f41d20d9e4b
ThijsieNL <thijs.olijerhoek@hotmail.com>
2022-08-22 14:08:51 +0200

    added comments for clarity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  //dit zijn de routes waar je naar kan verwijzen.
+  //ik verwijs naar /about op de home page en andersom. hieronder schrijf je dit natuurlijk zonder /
+  //je moet ook verwijzen naar het component wat je moet laden
   {
     path: 'home',
     component: HomeComponent //deze moet je wel importeren vanuit de home folder, import errors krijg je automatisch en zijn makkelijk op te lossen
