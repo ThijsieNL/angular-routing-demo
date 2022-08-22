d711efffba4d932d9f38b36e6e1a57a77880194f
ThijsieNL <thijs.olijerhoek@hotmail.com>
2022-08-22 13:57:32 +0200

    routing added to the about-page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent //deze moet je wel importeren vanuit de home folder, import errors krijg je automatisch en zijn makkelijk op te lossen
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+[]
